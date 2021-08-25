4b8c00dcc2e4c3d0befddbd902ee213eee08ab15
ClaudiaCZ <claudiavcasco@gmail.comgit config --global user.email git config --global user.email claudiavcasco@gmail.comgit config --global user.email claudiavcasco@gmail.com>
2021-08-25 17:12:44 -0300

    first commint

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require('path');
+const user = require('./routes/user');
+const main = require('./routes/main');
+
+app.use(express.static('pablic'))
+
+app.use('/', user)
+app.use('/', main)
+
+app.listen(port, () => console.log('Server listening at ' + port));
